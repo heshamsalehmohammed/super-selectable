2d9fbfb99a7429c2b701d69805aeb338e54a86d0
Hesham Saleh <32981736+heshamsalehmohammed@users.noreply.github.com>
2023-01-23 13:19:14 +0200

    build 1.0.5

--- src/components/SelectableItem.jsx
+++ src/components/SelectableItem.jsx
@@ -4,20 +4,28 @@ import {context} from './context';
 
 const SelectableItem = React.memo(
   React.forwardRef((props, forwardedRef) => {
-    const {ItemComponent, index, customData} = props;
+    const {ItemComponent, isIgnored = false, index, customData} = props;
 
     const api = React.useContext(context);
     if (!api) throw 'Segment must used inside SelectableContainer component.';
     const ref = React.useRef(null);
     React.useLayoutEffect(() => {
-      ref.current.customData = customData;
-      api.subscribe(ref);
-      return () => {
-        api.unSubscribe(ref);
-      };
+      if (!isIgnored) {
+        ref.current.customData = customData;
+        api.subscribe(ref);
+        return () => {
+          api.unSubscribe(ref);
+        };
+      }
     }, [api]);
 
-    return <ItemComponent customData={customData} index={index} ref={mergeRefs([ref, forwardedRef])} />;
+    return (
+      <ItemComponent
+        customData={customData}
+        index={index}
+        ref={mergeRefs([ref, forwardedRef])}
+      />
+    );
   })
 );
 
